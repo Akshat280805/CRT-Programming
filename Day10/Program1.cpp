ad018ac2fe82d5b17162694e5e96215fe3421eb2
Your Name <your@email.com>
2026-05-10 10:04:39 +0530

    Normal Commit

--- Day10/Program1.cpp
+++ Day10/Program1.cpp
@@ -14,30 +14,39 @@ int main() {
     //1. length of string
     int n = str1.length();
     cout<< "Length of string is: " << n << endl;
+    
     //2. Append String
     cout << "After Appending String is"<< str1.append(" Student") << endl;
+    
     //3. Empty String
     cout << "Is String Empty: " << str2.empty() << endl;
+    
     //4. Concatenate String
     cout << "After Concatenating String is: " << str1 + " " + "Bachoo" << endl;
     cout << "After Concatenating String is: " << str1  << endl;
+    
     //5. pushback() method
     str2.push_back('U');
     cout << "After pushback() method: " << str2 << endl; //Prints "JECRCU"
+    
     //6. popback() method
     str2.pop_back();
     cout << "After popback() method: " << str2 << endl; //Prints "JECRC"
+    
     //7. Find Method
     int index = str1.find("ello");
     cout << "Index of 'ello' in str1: " << index << endl;
+    
     //8. Swap Method
     str1.swap(str2);
     cout << "After Swapping String is: " << endl;
     cout << "str1: " << str1 << endl; //Prints "JECRC"
     cout << "str2: " << str2 << endl; //Prints "Hellostudent"
+    
     //9. Substring Method
     string substr = str2.substr(5, 6); //5 is index and 6 is length of substring
     cout << "Substring of str2 from index 5 with length 6: " << substr << endl; 
+    
     //10. Getline Method
     string str3;
     //cin >> str3; 
