dd12ab61b0514e6caea50162f6c13d4e58e491e2
Your Name <your@email.com>
2026-05-11 10:04:23 +0530

    normal Commit

--- Day11/Program-1.cpp
+++ Day11/Program-1.cpp
@@ -10,6 +10,24 @@ int LinearSearch(int nums[], int n, int target)
             return i;
         }
     }
-    return -1; //Element not found.
+
+    return -1;
+}
+int main()
+{
+    int nums[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int target = 30;
+    int result = LinearSearch(nums, n, target);
+    if (result != -1)
+    {
+        cout << "Element found at index: " << result;
+    }
+    else
+    {
+        cout << "Element not found";
+    }
+
+    return 0;
 }
 
